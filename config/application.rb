0c3726dd3ac91fc091924ebc7b05c19877c1a67b
ngareas5 <naren.gaindhar92@gmail.com>
2020-07-19 16:21:59 +0530

    change in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module KoetterWoodApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoloader = :classic
     
   end
 end
