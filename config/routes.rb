5e370b4ff4ff6be3471a1c027fd3e995282022fb
ngareas5 <naren.gaindhar92@gmail.com>
2020-07-19 12:50:36 +0530

    route file changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
   devise_for :users, controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api , defaults: {format: 'json'} do
